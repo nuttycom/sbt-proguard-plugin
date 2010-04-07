0dec1f4a9cc3a5d1261566fa8f135fb3b96b1eb4
Kris Nuttycombe <kris@socialmedia.com>
2010-04-07 10:18:30 -0600

    Fixed missing -libraryjars argument to include rt.jar, heuristically determined from the system java.home property.

--- src/main/scala/ProguardProject.scala
+++ src/main/scala/ProguardProject.scala
@@ -10,6 +10,8 @@ object ProguardProject {
 abstract class ProguardProject(info: ProjectInfo) extends DefaultProject(info) {
   def minJarName = artifactBaseName + ".min.jar"
   def minJarPath = outputPath / minJarName
+  def rtJarPath = Path.fromFile(System.getProperty("java.home")) / "lib" / "rt.jar"
+
 
   def proguardOptions: List[String] = Nil
 
@@ -29,9 +31,9 @@ abstract class ProguardProject(info: ProjectInfo) extends DefaultProject(info) {
   }
 
   //def proguardInJars = runClasspath --- proguardExclude
-  def proguardInJars = ((compileClasspath +++ allDependencyJars) ** "*.jar") --- proguardExclude
-  def proguardExclude = libraryJarPath +++ mainCompilePath +++ mainResourcesPath +++ managedClasspath(Configurations.Provided) 
-  def libraryJarPath = Path.emptyPathFinder 
+  def proguardInJars      = ((compileClasspath +++ allDependencyJars) ** "*.jar") --- jarPath --- proguardExclude
+  def proguardExclude     = proguardLibraryJars +++ mainCompilePath +++ mainResourcesPath +++ managedClasspath(Configurations.Provided) 
+  def proguardLibraryJars = rtJarPath
 
   def proguardKeepLimitedSerializability = """
     -keepclassmembers class * implements java.io.Serializable {
@@ -47,16 +49,25 @@ abstract class ProguardProject(info: ProjectInfo) extends DefaultProject(info) {
 
   def proguardKeepAllScala = "keep class scala.** { *; }"
 
+  def proguardInJarsArg = {
+    val inPaths = proguardInJars.get.foldLeft(Map.empty[String, Path])((m, p) => m + (p.asFile.getName -> p)).values
+    "-injars" :: (List(jarPath.absolutePath).elements ++ inPaths.map(_.absolutePath+"(!META-INF/MANIFEST.MF)")).mkString(File.pathSeparator) :: Nil
+  }
+
+  def proguardOutJarsArg = "-outjars" :: minJarPath.absolutePath :: Nil
+
+  def proguardLibJarsArg = {
+    println(proguardLibraryJars.get)
+    val libPaths = proguardLibraryJars.get.foldLeft(Map.empty[String, Path])((m, p) => m + (p.asFile.getName -> p)).values
+    if (libPaths.hasNext) "-libraryjars" :: libPaths.mkString(File.pathSeparator) :: Nil else Nil
+  }
+
+  def proguardDefaultArgs = "-dontwarn" :: "-dontoptimize" :: "-dontobfuscate" :: proguardOptions 
+
   lazy val proguard = proguardAction
   def proguardAction = proguardTask dependsOn(`package`) describedAs(ProguardProject.Description)
   def proguardTask = task {
-    val inPaths = proguardInJars.get.foldLeft(Map.empty[String, Path])((m, p) => m + (p.asFile.getName -> p)).values
-
-    val args = "-injars" :: inPaths.map(_.absolutePath+"(!META-INF/MANIFEST.MF)").mkString(File.pathSeparator) ::
-               "-outjars" :: minJarPath.absolutePath ::
-               "-dontwarn" :: "-dontoptimize" :: "-dontobfuscate" :: 
-               proguardOptions 
-
+    val args = proguardInJarsArg ::: proguardOutJarsArg ::: proguardLibJarsArg ::: proguardDefaultArgs
     val config = new ProGuardConfiguration
     new ConfigurationParser(args.toArray[String], info.projectPath.asFile).parse(config)
     new ProGuard(config).execute
